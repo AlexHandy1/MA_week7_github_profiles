4606192002104163fbd000aee5f61c6344bd4481
Alex Handy <handy437@gmail.com>
2015-06-08 16:55:55 +0100

    Added API call and test

--- js/controllers/gitUserSearchController.js
+++ js/controllers/gitUserSearchController.js
@@ -1,20 +1,12 @@
-gituserSearch.controller('GitUserSearchController', [function() {
+gituserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+  var searchResource = $resource('https://api.github.com/search/users')
+  var token = '736ec2b220d58eefcaaa306b189c798e4981ceb5'
   var self = this
 
   self.doSearch = function(){
-    self.searchResult = {
-    "items": [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-     ]
+    self.searchResult = searchResource.get(
+      {q: self.searchTerm,
+      access_token: token}
+      );
     }
-  }
 }]);
