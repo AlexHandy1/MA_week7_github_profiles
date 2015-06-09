a723cf49acb431212518fd45e382669397895317
Alex Handy <handy437@gmail.com>
2015-06-09 17:14:32 +0100

    Attempt at chaining approach

--- js/controllers/gitUserSearchController.js
+++ js/controllers/gitUserSearchController.js
@@ -2,10 +2,20 @@ gituserSearch.controller('GitUserSearchController', ['Search', function(Search) 
   var self = this
 
   self.doSearch = function(){
+      var counter = 0
+      self.logins = []
       Search.query(self.searchTerm)
       .then(function(response) {
-        self.searchResult = [response.data];
-        console.log(response.data)
+        self.searchResult = response.data;
+        console.log(response.data.items[counter].login);
+        self.logins.push(response.data.items[counter].login)
+        console.log(self.logins)
+        counter +=1;
+      })
+      Search.queryTwo(self.logins)
+      .then(function(response) {
+        self.extraInfo = response.data;
+        console.log(response.data);
       })
     };
 }]);
--- js/services/searchFactory.js
+++ js/services/searchFactory.js
@@ -1,26 +1,24 @@
 gituserSearch.factory('Search', ['$http', function($http) {
-  var queryUrl = 'https://api.github.com/users'
+  var queryUrl = 'https://api.github.com/search/users?access_params=736ec2b220d58eefcaaa306b189c798e4981ceb5'
 
-  // 'https://api.github.com/search/users?access_params=736ec2b220d58eefcaaa306b189c798e4981ceb5'
-  // var queryUrl2 = 'https://api.github.com/search/users?access_params=736ec2b220d58eefcaaa306b189c798e4981ceb5'
-
-  // "https://api.github.com/users?"
+  var userQueryUrl = 'https://api.github.com/users'
 
   return {
     query: function(searchTerm) {
       return $http({
-        url: queryUrl + "/" + searchTerm,
+        url: queryUrl,
+        method: 'GET',
+        params: {
+          'q': searchTerm
+        }
+      });
+    }
+
+    queryTwo: function(logins) {
+      return $http({
+        url: userQueryUrl + "/" + logins,
         method: 'GET',
       });
     }
   }
-
-  //   query2: function(searchTerm) {
-  //     return $http({
-  //       url: queryUrl2
-  //       method: 'GET'
-  //     })
-  //   }
-  // }
-
 }]);
