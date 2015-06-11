db47e4ccbd609cb60a2cde382782c60f1433855e
Alex Handy <handy437@gmail.com>
2015-06-11 11:24:32 +0100

    Working form writing to Mongo database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 var app = express();
 var path = require('path');
 
@@ -12,6 +14,13 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// think I do need this but what exactly doing
 app.use(function(req,res,next){
     req.db = db;
     next();
@@ -33,6 +42,32 @@ app.get('/mongo', function(req, res) {
         });
     });
 })
+
+app.get('/newuser', function(req, res) {
+  res.render('newuser', {title: "Add New User"});
+});
+
+app.post('/adduser', function(req, res) {
+  var db = req.db;
+  console.log(req.body);
+  console.log(req.params);
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  console.log(firstName);
+  console.log(lastName);
+
+  var collection = db.get('customers')
+
+  collection.insert(
+    {"firstName" : firstName, "lastName" : lastName},
+    function(err, doc) {
+      if(err) {
+        res.send("There was a problem adding the information to the database.");
+      } else {
+        res.redirect("mongo");
+      }
+    });
+});
 
 app.get('/apitoken', function(req, res){
   res.json({token: '736ec2b220d58eefcaaa306b189c798e4981ceb5'});
